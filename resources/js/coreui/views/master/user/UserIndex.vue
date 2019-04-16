20241878c29e6c3fe271ff99ade90caa43571208
Rahmat Ade Saputra <gen781@gmail.com>
2019-04-16 16:47:19 +0700

    update router & tampilan modul

--- resources/js/coreui/views/master/user/UserIndex.vue
+++ resources/js/coreui/views/master/user/UserIndex.vue
@@ -11,37 +11,13 @@
         >
           <template slot="caption">
             <i class="fa fa-align-justify" /> Daftar User
-            <!-- <div class="pull-right">
-            <div >
-              <b-button
-                variant="success"
-                to="user/create"
-              >
-                <i class="fa fa-plus" />
-                Tambah User
-              </b-button>
-            </div>
-            <div>
-              <b-input-group>
-                <b-form-input
-                  type="text"
-                  placeholder="Cari"
-                />
-                <b-input-group-append>
-                  <b-input-group-text>
-                    <i class="fa fa-search" />
-                  </b-input-group-text>
-                </b-input-group-append>
-              </b-input-group>
-            </div>
-            </div> -->
-
             <div class="row">
               <div class="col-md-4 col-sm-12 mt-2">
                 <b-input-group>
                   <b-form-input
                     type="text"
                     placeholder="Cari"
+                    v-model="cariUser"
                   />
                   <b-input-group-append>
                     <b-input-group-text>
@@ -77,28 +53,22 @@ import UserTable from './UserTable.vue'
 export default {
   name      : 'UserIndex',
   components: { UserTable },
-  // data() {
-  //   return {
-  //     users: '',
-  //   }
-  // },
-  // methods: {
-  //   getUsers() {
-  //     axios.get('/api/user',{
-  //       headers: {
-  //         'Authorization': 'Bearer ' + localStorage.getItem('token'),
-  //         'Content-Type': 'application/json'
-  //       }
-  //     }).then(response => {
-  //         this.users=response.data.users;
-  //         console.log(this.users);
-  //     }).catch(error => {
-  //         console.log(error);
-  //     });
-  //   }
-  // },
-  // mounted() {
-  //   this.getUsers();
-  // }
+  data() {
+    return {
+      cariUser: '',
+    }
+  },
+  methods: {
+    getUser(user) {
+      axios.get('/api/user/'+user).then(response => {
+        this.cariUser = response.data.user;
+        // console.log(this.user);
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  mounted() {
+  }
 }
 </script>
