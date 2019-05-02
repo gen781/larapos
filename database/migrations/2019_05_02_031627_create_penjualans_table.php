3944a5c7d19138d783155aa7b22b1894ff74c842
Rahmat Ade Saputra <gen781@googlemail.com>
2019-05-02 10:32:30 +0700

    update penjualan

---
+++ database/migrations/2019_05_02_031627_create_penjualans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePenjualansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('no_trans')->unique();
+            $table->string('tgl_trans');
+            $table->integer('pelanggan');
+            $table->integer('user');
+            $table->integer('sub_total');
+            $table->integer('diskon');
+            $table->integer('total_harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjualans');
+    }
+}
